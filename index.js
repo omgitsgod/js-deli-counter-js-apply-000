f12359fed2d56cd0a8548996ab027a1269d8940e
Ryan Travis <rpenchenski@gmail.com>
2018-06-27 23:18:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var katzDeliLine = []
 
 function takeANumber(katzDeliLine, name) {
-  return `Welcome, ${name}. You are number ${katzDeliLine.indexOf(name)} in line.`
+  return `Welcome, ${name}. You are number ${katzDeliLine.indexOf(name) +1} in line.`
 }
 
 function nowServing() {
