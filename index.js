16f4c8c3581af6c01d6d00f0b4d234a3374a4f07
Ryan Travis <rpenchenski@gmail.com>
2018-06-28 00:30:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "The line is currently empty."
   } else {
-    for (i = 0; i < katzDeliLine.length; i++)
     return `The line is currently: 1. ${katzDeliLine[0]}, 2. ${katzDeliLine[1]}`
   }
 }
