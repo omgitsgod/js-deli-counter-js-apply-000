2dfc07041700e3c5486c668e0f07b6a561889194
Ryan Travis <rpenchenski@gmail.com>
2018-06-28 00:06:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ function takeANumber(katzDeliLine, name) {
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
-  return `Currently serving ${katzDeliLine[0]}.`
-  katzDeliLine.shift()
+    var current = katzDeliLine.shift()
+  return `Currently serving ${current}.`
+  
     
   } else {
     return "There is nobody waiting to be served!"
