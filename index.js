143b4288170bbe6e26ffcaedca0c60728e54c9dd
Ryan Travis <rpenchenski@gmail.com>
2018-06-28 00:13:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ function nowServing(katzDeliLine) {
 }
 function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
-    return "The line is currently empty"
+    return "The line is currently empty."
   }
 }
